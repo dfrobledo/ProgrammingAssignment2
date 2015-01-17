8a4364f84aee3f5d73ace99054da37041fd1063e
DFRobledo <diego.robledo@gmail.com>
2015-01-16 21:26:48 -0500

    A Better version of the functions

--- cachematrix.R
+++ cachematrix.R
@@ -9,11 +9,13 @@ makeCacheMatrix <- function(Values = Matrix())
 
     Mat <- NULL
     
+    ##Function to manage the values
     Set <- function(x) 
     {
         Values <<- x
     }
     
+    ##Prepare the Get and Set functions
     Get <- function() Values
     SetInv <- function(Inv) Mat <<- Inv
     GetInv <- function() Mat
@@ -30,16 +32,18 @@ cacheSolve <- function(x)
 ## Return a matrix that is the inverse of 'x'
 
     ## DR: If we know the inverse of the matrix, we read the cache, else, calculate it!
-    
+    ## Let's try to get the cache
     InvMat <- x$GetInv()
-
+    ## The cache exists? Then return it
     if (!is.null(InvMat)) 
     {
         return(InvMat)
     }
+    ## Else, calculate it an set the according function to generate the cache
     InvMat <- solve(x$Get())
     x$SetInv(InvMat)
     
+    ##Return the calculation
     return(InvMat)
     
 }
